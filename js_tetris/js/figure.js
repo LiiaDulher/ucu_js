6c8373f0f3ee5758d557d8cf80b556fcaabe6f54
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2021-04-10 20:39:13 +0300

    Update figure.js

--- js_tetris/js/figure.js
+++ js_tetris/js/figure.js
@@ -1,6 +1,7 @@
 // falling - new figure is put on the playgound
 // static - figure stoped moving. This happens when there are obsicles for any cells bellow
 function Figure(obsticles, state = STATES.FALLING) {
+
   // Public properties
   this.cells = [];
   this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
@@ -8,6 +9,24 @@ function Figure(obsticles, state = STATES.FALLING) {
   this.state = state;
   this.obsticles = obsticles;
 
+  const generateShape = () =>
+    FIGURE_SHAPES[Math.floor(Math.random() * FIGURE_SHAPES.length)];
+
+  this.shape = generateShape();
+
+  const generateRotation = () => Math.floor(Math.random() * FIGURE_ROTATIONS[this.shape].length);
+
+  this.rotaion_state = generateRotation();
+
+  const generateCenter = () => {
+    let y = FIGURE_CENTER[this.shape][this.rotaion_state][0];
+    let x = Math.floor(Math.random() * FIGURE_CENTER[this.shape][this.rotaion_state][1][1]) +
+              FIGURE_CENTER[this.shape][this.rotaion_state][1][0];
+    return [y, x];
+  };
+
+  this.center = generateCenter();
+  
   // Private methods
   const validFor = (direction) =>
     this.cells.every(cell => cell.validFor(direction));
@@ -15,40 +34,49 @@ function Figure(obsticles, state = STATES.FALLING) {
   // initialise figure cells
   const addCell = (x, y) =>
     this.cells.push(new Cell(x, y, this.color, this.id, this.obsticles, this.state));
-
-  const generateCoordinates = () =>
-    INITIAL_POSITIONS[Math.floor(Math.random() * INITIAL_POSITIONS.length)];
+  
+  const generateCoordinates = () => {
+    return FIGURE_ROTATIONS[this.shape][this.rotaion_state]
+  }
 
   generateCoordinates().forEach(([y, x]) =>
-    addCell(x, y));
+    addCell(x+this.center[1], y+this.center[0]));
 
   // Public methods
-  this.moveDown = () => {
-    if (validFor(DOWN)) {
-      this.cells.forEach(cell => cell.deRender());
-      this.cells.forEach(cell => cell.moveDown());
-    } else {
-      this.state = STATES.STATIC
+  this.move = (direction) => {
+    if (direction === ROTATE_CLOCKWISE || direction === ROTATE_COUNTERCLOCKWISE){
+      this.rotate(direction);
+      return;
     }
-  };
+    if (validFor(direction)){  
+      this.cells.forEach(cell => cell.deRender());
+      this.cells.forEach(cell => cell.move(direction));
+      return;
+    }
+    if (direction === DOWN){
+      this.state = STATES.STATIC;
+    }
+  }
 
-  this.moveRight = () => { // TODO: move right and move left are almost identical
-    if (!validFor(RIGHT)) return;
-
-    this.cells.forEach(cell => cell.deRender());
-    this.cells.forEach(cell => cell.moveRight());
-  };
-
-  this.moveLeft = () => {
-    if (!validFor(LEFT)) return;
-
-    this.cells.forEach(cell => cell.deRender());
-    this.cells.forEach(cell => cell.moveLeft());
-  };
-
-  this.rotate = () => {
-    // TODO: this is complicated. But really can be solved with basic math.
-    //       make sure you are rotating around the center element
-    //       all figures will be eaither 3 cells wide or 3 cells hight
+  this.rotate = (direction) => {
+    const rotation_direction = (direction === ROTATE_CLOCKWISE) ? 1 : -1;
+    this.center = [this.cells[0].x, this.cells[0].y];
+    console.log(this.center)
+    this.rotaion_state = (this.rotaion_state + rotation_direction < 0) ?
+                        (this.rotaion_state + rotation_direction+FIGURE_ROTATIONS[this.shape].length)%FIGURE_ROTATIONS[this.shape].length : 
+                        (this.rotaion_state + rotation_direction)%FIGURE_ROTATIONS[this.shape].length;
+    let cells = generateCoordinates();
+    let validator = true;
+    for (let i=0; i < cells.length; i++){
+        validator &&= this.cells[i].validFor(direction, cells[i][1]+this.center[0], cells[i][0]+this.center[1])
+    }
+    if (validator){
+      this.cells.forEach(cell => cell.deRender());
+      for (let i=0; i < cells.length; i++){
+        this.cells[i].move(direction, cells[i][1]+this.center[0], cells[i][0]+this.center[1])
+      }
+      return;
+    }
+    this.rotaion_state = (this.rotaion_state - rotation_direction)%FIGURE_ROTATIONS[this.shape].length;
   }
 }
