6d3cbcf65c98964cf5c2d176ff55c4966815109a
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2021-04-10 20:38:50 +0300

    Update constants.js

--- js_tetris/js/constants.js
+++ js_tetris/js/constants.js
@@ -13,19 +13,49 @@ const STATES = {
   STATIC: 'STATIC'
 };
 
-// default positions for objects
-// TODO: would be nice to be able to "generate" an object. There must be some generic rules?
-const INITIAL_POSITIONS = [
-  [[PLAYGROUND_HEIGHT + 1, 1], [PLAYGROUND_HEIGHT, 1], [PLAYGROUND_HEIGHT, 2], [PLAYGROUND_HEIGHT, 3]],
-  [[PLAYGROUND_HEIGHT + 2, 2], [PLAYGROUND_HEIGHT + 1, 2], [PLAYGROUND_HEIGHT, 2]],
-  [[PLAYGROUND_HEIGHT + 2, 3], [PLAYGROUND_HEIGHT + 1, 3], [PLAYGROUND_HEIGHT, 3]],
-];
+const FIGURE_SHAPES = ['L', 'T', 'I'];
+
+const FIGURE_ROTATIONS = {
+  'L':[ [[0,0], [1,-1],  [0,-1], [0,1]],
+        [[0,0], [1,1],   [1,0],  [-1,0] ],
+        [[0,0], [-1,1],  [0,1],  [0,-1]],
+        [[0,0], [-1,-1], [-1,0], [1,0]] 
+      ],
+  'T':[ [[0,0], [0,-1], [-1,0], [0,1]],
+        [[0,0], [1,0],  [0,-1], [-1,0]],
+        [[0,0], [0,1],  [1,0],  [0,-1]],
+        [[0,0], [-1,0], [0,1],  [1,0]]
+      ],
+  'I':[ [[0, 0], [-1, 0], [1, 0]],
+        [[0, 0], [0, 1],  [0, -1]],
+        [[0, 0], [1, 0],  [-1, 0]],
+        [[0, 0], [0, -1], [0, 1]]
+      ]
+};
+
+const FIGURE_CENTER = {
+  'L': [[PLAYGROUND_HEIGHT, [1, PLAYGROUND_WIDTH-3]], 
+        [PLAYGROUND_HEIGHT + 1, [0, PLAYGROUND_WIDTH-2]], 
+        [PLAYGROUND_HEIGHT + 1, [1, PLAYGROUND_WIDTH-3]], 
+        [PLAYGROUND_HEIGHT + 1, [1, PLAYGROUND_WIDTH-2]]
+      ],
+  'T': [[PLAYGROUND_HEIGHT + 1, [1, PLAYGROUND_WIDTH-3]],
+        [PLAYGROUND_HEIGHT + 1, [0, PLAYGROUND_WIDTH-2]],
+        [PLAYGROUND_HEIGHT, [1, PLAYGROUND_WIDTH-3]],
+        [PLAYGROUND_HEIGHT + 1, [0, PLAYGROUND_WIDTH-2]]
+      ],
+  'I': [[PLAYGROUND_HEIGHT + 1, [0, PLAYGROUND_WIDTH-1]],
+       [PLAYGROUND_HEIGHT, [1, PLAYGROUND_WIDTH-3]]
+      ]
+};
 
 // key codes for keydown events
 const DOWN = 40;
 const RIGHT = 39;
 const LEFT = 37;
 const PAUSE = 32;
+const ROTATE_CLOCKWISE = 68;
+const ROTATE_COUNTERCLOCKWISE = 65;
 
 // Game constants
 const INTERVAL = 1000;
